666057e67ad34b278d38b14fe2654b9b3d00843b
Indra Nand Jha <indranandjha0@gmail.com>
2023-12-22 23:50:31 +0530

    add domain verifier

---
+++ app/validation/domain_verifier.py
@@ -0,0 +1,10 @@
+import dns.resolver
+
+
+def validate_domain(email):
+    domain = email.split('@')[1]
+    try:
+        dns.resolver.resolve(domain, 'MX')
+        return True
+    except (dns.resolver.NoAnswer, dns.resolver.NXDOMAIN, dns.exception.Timeout):
+        return False
